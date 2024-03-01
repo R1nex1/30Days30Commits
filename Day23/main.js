beac5e2864334ecbaae0b7a4ef202d9fa1a5e909
R1nex1 <kaspar.shanin@gmail.com>
2024-03-01 09:38:10 +0200

    Recipe finder

---
+++ Day23/main.js
@@ -0,0 +1,32 @@
+const searchForm = document.querySelector(".search-form");
+const searchInput = document.querySelector(".search");
+const resultsList = document.querySelector(".result");
+
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    searchRecipes();
+});
+
+async function searchRecipes() {
+    const searchValue = searchInput.value.trim();
+    const response = await fetch(`https://api.edamam.com/search?q=${searchValue}&app_id=YOUR-APP-ID-HERE&app_key=YOUR-API-KEY-HERE&from=0&to=15`);
+    const data = await response.json();
+    displayRecipes(data.hits);
+}
+
+function displayRecipes(recipes) {
+    let html = '';
+    recipes.forEach((recipe) => {
+        html += `
+        <div>
+            <img src="${recipe.recipe.image}" alt="${recipe.recipe.label}">
+            <h3>${recipe.recipe.label}</h3>
+            <ul>
+                ${recipe.recipe.ingredientLines.map(ingredient => `<li>${ingredient}</li>`).join('')}
+            </ul>
+            <a href="${recipe.recipe.url}" target="_blank">View Recipe</a>
+        </div> 
+        `
+    })
+    resultsList.innerHTML = html;
+}
